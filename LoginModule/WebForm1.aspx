68f8786523d5124f242aece859e80ea1642c9e2d
Dell <Dell@DESKTOP-5NOVF6P>
2021-04-07 14:04:23 +0530

    Hello

---
+++ LoginModule/WebForm1.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="LoginModule.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml"> 
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="font-family: 'Comic Sans MS'; font-size: xx-large; font-weight: bold; font-style: oblique; color: #000080">
+            <asp:Label ID="Label1" runat="server" Text="Username :"></asp:Label>
+            <asp:TextBox ID="txtusername" runat="server" Width="172px"></asp:TextBox>
+        </div>
+        <p style="font-family: 'Comic Sans MS'; font-size: xx-large; font-weight: bold; font-style: oblique; color: #000080">
+            <asp:Label ID="Label2" runat="server" Text="Password :" TextMode="Password"></asp:Label>
+            <asp:TextBox ID="txtPassword" runat="server" style="margin-left: 9px" Width="174px"></asp:TextBox>
+            </p>
+        <p>
+            <asp:Button ID="Button1" runat="server" Text="Login" Height="43px" style="margin-left: 42px" Width="178px" OnClick="Button1_Click" />
+            </p>
+        <p style="font-family: 'Comic Sans MS'; font-size: xx-large; font-weight: bold; font-style: oblique; color: #000080">
+            <asp:Label ID="lblErrorMessage" runat="server" Text="Wrong credential"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ LoginModule/WebForm1.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LoginModule
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblErrorMessage.Visible = false;
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-5NOVF6P;Initial Catalog=userDetails;Integrated Security=True;")) 
+            {
+                con.Open();
+;               string query = "select * from usertbl where username=@username and password=@password";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@username", txtusername.Text);
+                cmd.Parameters.AddWithValue("@password", txtPassword.Text);
+                int count = Convert.ToInt32(cmd.ExecuteScalar());
+                if (count > 0)
+                {
+                    Session["username"] = txtusername.Text;
+                    Response.Redirect("dashboard.aspx");
+
+
+                }
+                else
+                {
+                    lblErrorMessage.Visible = true;
+                }
+
+
+
+            }
+
+
+        }
+    }
+}
